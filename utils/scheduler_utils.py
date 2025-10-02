42b63199f3e9617d036e12eb69bc23239ca45a6a
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2025-10-02 10:39:36 +0200

    SchedulerUtils now returns the channel_indexes

--- utils/scheduler_utils.py
+++ utils/scheduler_utils.py
@@ -10,8 +10,8 @@ class SchedulerUtils:
     def get_valid_schedules(schedule_plan: List[Schedule], instance_data: InstanceData, schedule_time: int):
         valid_channels = []
 
-        for channel_index, channel in enumerate(instance_data.channels):
+        for channel_index, _ in enumerate(instance_data.channels):
             if Validator.is_channel_valid(schedule_plan, instance_data, channel_index, schedule_time):
-                valid_channels.append(channel)
+                valid_channels.append(channel_index)
 
         return valid_channels
