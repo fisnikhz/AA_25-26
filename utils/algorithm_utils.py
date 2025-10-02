22f7698f0790ae08820a3780c201b6ecd2bc6b44
Florian Saqipi <49369376+floriansaqipi@users.noreply.github.com>
2025-10-02 12:32:50 +0200

    Added algorithm utils

---
+++ utils/algorithm_utils.py
@@ -0,0 +1,74 @@
+from typing import List
+
+from models.channel import Channel
+from models.instance_data import InstanceData
+from models.program import Program
+from models.schedule import Schedule
+from utils.utils import Utils
+from validator.validator import Validator
+
+
+class AlgorithmUtils:
+
+    @staticmethod
+    def get_valid_schedules(schedule_plan: List[Schedule], instance_data: InstanceData, schedule_time: int):
+        valid_channels = []
+
+        for channel_index, _ in enumerate(instance_data.channels):
+            if Validator.is_channel_valid(schedule_plan, instance_data, channel_index, schedule_time):
+                valid_channels.append(channel_index)
+
+        return valid_channels
+
+    @staticmethod
+    def get_best_fit(schedule_plan: List[Schedule], instance_data: InstanceData, schedule_time: int,
+                     valid_channel_indexes: List[int]): #returns best channel to pick at the time and what score will it provide if we switch to it
+        max_score = 0
+        best_channel = None
+
+        for channel_index in valid_channel_indexes:
+            channel = instance_data.channels[channel_index]
+            program = Utils.get_channel_program_by_time(channel, schedule_time)
+
+            if not program:
+                continue
+
+            score = 0
+
+            score += program.score
+            score += AlgorithmUtils.get_time_preference_bonus(instance_data, program, schedule_time)
+            score += AlgorithmUtils.get_switch_penalty(schedule_plan, instance_data, channel) # qetu kam me shtu late schedule penalty, early termination penalty
+
+
+
+            if score > max_score:
+                max_score = score
+                best_channel = channel
+
+        return best_channel, max_score
+
+
+    @staticmethod
+    def get_time_preference_bonus(instance_data: InstanceData, program: Program, schedule_time: int):
+        score = 0
+        for preference in instance_data.time_preferences:
+            if program.genre == preference.preferred_genre and preference.start <= schedule_time < preference.end:
+                score += preference.bonus
+
+        return score
+
+    @staticmethod
+    def get_switch_penalty(schedule_plan: List[Schedule], instance_data: InstanceData, channel: Channel):
+        penalty = 0
+        if not schedule_plan:
+            return penalty
+
+        last_schedule = schedule_plan[-1]
+        if last_schedule.channel_id != channel.channel_id:
+            penalty -= instance_data.switch_penalty
+
+        return penalty
+
+    @staticmethod
+    def get_delay_penalty(schedule_plan: List[Schedule], instance_data: InstanceData, channel: Channel):
+        pass
